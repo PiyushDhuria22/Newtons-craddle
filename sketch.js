8bd9cc7aacc6a6fa724dc509ae851dda43213892
PiyushDhuria22 <75304779+PiyushDhuria22@users.noreply.github.com>
2021-06-12 17:42:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,14 +21,14 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	bob1 = new Bob(100,300,20);
-	bob2 = new Bob(200,400,20);
-	bob3 = new Bob(300,400,20);
-	bob4 = new Bob(400,400,20);
-	bob5 = new Bob(500,400,20);
+	bob1 = new Bob(50,400,20);
+	bob2 = new Bob(100,400,20);
+	bob3 = new Bob(150,400,20);
+	bob4 = new Bob(200,400,20);
+	bob5 = new Bob(250,400,20);
 	roofobject=new Roof(400,200,500,30);
-	rope1=new Rope(bob1.body,roofobject.body,-50,0);
-	rope2=new Rope(bob2.body,roofobject.body,-100,0);
+	rope1=new Rope(bob1.body,roofobject.body,-100,0);
+	rope2=new Rope(bob2.body,roofobject.body,-50,0);
 	rope3=new Rope(bob3.body,roofobject.body,0,0);
 	rope4=new Rope(bob4.body,roofobject.body,50,0);
 	rope5=new Rope(bob5.body,roofobject.body,100,0);
@@ -60,7 +60,9 @@ function draw() {
  
 }
 function keyPressed(){
-	
+	if(keyDown("UP_ARROW")){
+    Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-70,y:155})
+	}
 }
 
 
